e5c85da1189d279566b492ed25a97ba906ec181f
Nydus Born <nydusborn@outlook.com>
2022-02-09 11:59:32 +0300

    Basic scaffolding for functions that might be required.

--- ClassLibraryNumberTranslator/NumberTranslator.cs
+++ ClassLibraryNumberTranslator/NumberTranslator.cs
@@ -11,6 +11,31 @@ namespace ClassLibraryNumberTranslator
     /// </summary>
     public class NumberTranslator
     {
+        public static string[] Splitter (string To_Split)
+        {
+            string[] arr = new string[2];
+            To_Split = To_Split.Split(".");
+            return arr;
+        }
+        public static long FromTo10_Left (string In_Bx)
+        {
 
+        } 
+        public static string From10To_Left (long In_B10)
+        {
+
+        } 
+        public static double FromTo10_Right(string In_Bx)
+        {
+
+        }
+        public static string From10To_Right (long In_B10)
+        {
+
+        } 
+        public static string Merger (string A, string B)
+        {
+            return A + "." + B;
+        }
     }
 }
