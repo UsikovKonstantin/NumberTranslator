23d972c8dd7898ff62ce0f9e2a913305e45d737b
UsikovKonstantin <99312736+UsikovKonstantin@users.noreply.github.com>
2022-02-09 09:47:47 +0300

    Добавьте файлы проекта.

---
+++ ClassLibraryNumberTranslator/NumberTranslator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryNumberTranslator
+{
+    /// <summary>
+    /// Класс для перевода вещественных чисел из одной системы счисления в другую
+    /// </summary>
+    public class NumberTranslator
+    {
+
+    }
+}
