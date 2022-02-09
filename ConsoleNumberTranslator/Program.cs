2275d0aa70b26c55d85ac25c365d9b3acc0c5859
Nydus Born <nydusborn@outlook.com>
2022-02-09 13:00:08 +0300

    Tests for Splitter and Merger.

--- ConsoleNumberTranslator/Program.cs
+++ ConsoleNumberTranslator/Program.cs
@@ -12,7 +12,21 @@ namespace ConsoleNumberTranslator
     {
         static void Main(string[] args)
         {
-
+            Splitter_Test();
+            Merger_Test();
+        }
+        static void Splitter_Test()
+        {
+            string[] arr1, arr2;
+            arr1 = NumberTranslator.Splitter("125.74");
+            arr2 = new string[] { "125","74"};
+            Debug.Assert(arr1[0] == arr2[0] && arr1[1] == arr2[1]);
+        }
+        static void Merger_Test()
+        {
+            string[] arr = new string[] { "547", "214" };
+            string str = NumberTranslator.Merger(arr[0],arr[1]);
+            Debug.Assert(str == "547.214");
         }
     }
 }
