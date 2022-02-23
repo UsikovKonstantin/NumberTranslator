c0264caf41162d8cb0e12f5e1bcd655a2c685c74
ArtyomI69 <Artyom655@yandex.ru>
2022-02-23 22:49:17 +0300

    Добавил первый unit тест

---
+++ UnitTestsNumberTranslator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestsNumberTranslator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestsNumberTranslator")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2bf6d88f-1c9e-4e89-9c2f-131babb49bfc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestsNumberTranslator/UnitTestsNumberTranslator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryNumberTranslator;
+
+namespace UnitTestsNumberTranslator {
+    [TestClass]
+    public class UnitTestsNumberTranslator {
+        [TestMethod]
+        public void FromPto10Int_ReturnsNumberInBase10() {
+            bool test1 = NumberTranslator.FromPto10Int("101", 2) == "5";
+            bool test2 = NumberTranslator.FromPto10Int("10", 2) == "2";
+            Assert.IsTrue(test1 && test2);
+        }
+    }
+}
