7ebf39b232ea5ae8601f6aea25fec106c830df92
Toropyga <ilia.spiridonoff2017@yandex.ru>
2022-02-23 23:19:01 +0300

    test

--- ConsoleNumberTranslator/ConsoleTesting.cs
+++ ConsoleNumberTranslator/ConsoleTesting.cs
@@ -8,6 +8,7 @@ namespace ConsoleNumberTranslator
     {
         static void Main()
         {
+            Console.WriteLine("1");
             Console.WriteLine("Test");
             FromPto10Int_Test();
             From10toQInt_Test();
